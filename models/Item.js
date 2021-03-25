bfe641804b8937ab22a091009c047f9491d2659d
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-25 19:48:50 +0700

    +model item

---
+++ models/Item.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const { Objectid } = mongoose.Schema;
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        default: "Indonesia"
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    isPopular: {
+        type: Boolean
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageId: {
+        type: Objectid,
+        ref: 'Image'
+    },
+    featureId: {
+        type: Objectid,
+        ref: 'Feature'
+    },
+    activityId: {
+        type: Objectid,
+        ref: 'Activity'
+    },
+
+});
+
+module.exports = mongoose.model("Item", itemSchema);
