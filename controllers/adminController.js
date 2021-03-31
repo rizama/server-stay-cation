cf03697125a3d5c147b048df833f950c823ca49f
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-31 20:08:26 +0700

    ^ add method view category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,9 @@
 module.exports = {
-    index: (req, res) => {
+    viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard');
+    },
+
+    viewCategory: (req, res) => {
+        res.render('admin/category/view_category');
     }
 }
