2c7e725455e81b9ca7baabd557585aaccf739631
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-01 19:40:48 +0700

    ^register bank routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,6 @@ const adminController = require("../controllers/adminController");
 
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/categories', adminController.viewCategory);
+router.get('/bank', adminController.viewBank);
 
 module.exports = router;
