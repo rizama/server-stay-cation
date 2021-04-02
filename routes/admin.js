358f1cc025762e8bb3086bb4b45e48a1b8cfccc0
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-02 08:31:31 +0700

    ^ add routes booking and item

--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,7 @@ const adminController = require("../controllers/adminController");
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/categories', adminController.viewCategory);
 router.get('/bank', adminController.viewBank);
+router.get('/items', adminController.viewItem);
+router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
