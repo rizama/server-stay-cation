2c73b17045818e2abad76d49aad47138bf7ab08c
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-02 08:31:46 +0700

    ^controller items and booking

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,5 +9,13 @@ module.exports = {
 
     viewBank: (req, res) => {
         res.render('admin/bank/view_bank');
+    },
+
+    viewItem: (req, res) => {
+        res.render('admin/item/view_item');
+    },
+
+    viewBooking: (req, res) => {
+        res.render('admin/booking/view_booking');
     }
 }
