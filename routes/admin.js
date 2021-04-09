7831860ca3da5749d439b85f108584d5f3f3a886
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-09 10:47:55 +0700

    ^route update category

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const adminController = require("../controllers/adminController");
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/categories', adminController.viewCategory);
 router.post('/categories', adminController.addCategory);
+router.put('/categories', adminController.updateCategory);
 router.get('/bank', adminController.viewBank);
 router.get('/items', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);
