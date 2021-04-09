b057bab017acf2f0a9c0ac085416c25554eedb95
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-09 13:31:46 +0700

    ^ route delete category

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const adminController = require("../controllers/adminController");
 
+// Dashboard
 router.get('/dashboard', adminController.viewDashboard);
+
+// Category
 router.get('/categories', adminController.viewCategory);
-router.post('/categories', adminController.addCategory);
+router.post('/categories', adminController.storeCategory);
 router.put('/categories', adminController.updateCategory);
+router.delete('/categories/:id', adminController.destroyCategory);
+
 router.get('/bank', adminController.viewBank);
 router.get('/items', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);
