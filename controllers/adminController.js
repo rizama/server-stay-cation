f0992d8bcbec18cac291fec21dd5a9899c5de438
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-05-05 05:43:40 +0700

    ✍ add method delete feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -426,6 +426,33 @@ module.exports = {
             res.redirect(`/admin/items/show-detail-item/${itemId}/`);
         }
     },
+    destroyFeature: async (req, res) => {
+        const { id, itemId } = req.params;
+        try {
+            const feature = await Feature.findOne({ _id: id });
+
+            const item = await Item.findOne({ _id: itemId }).populate(
+                'featureId'
+            );
+            for (let i = 0; i < item.featureId.length; i++) {
+                if (
+                    item.featureId[i]._id.toString() === feature._id.toString()
+                ) {
+                    item.featureId.pull({ _id: feature._id });
+                    await item.save();
+                }
+            }
+            await fs.unlink(path.join(`public/${feature.imageUrl}`));
+            await feature.remove();
+            req.flash('alertMessage', 'Success Delete Feature');
+            req.flash('alertStatus', 'success');
+            res.redirect(`/admin/items/show-detail-item/${itemId}`);
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect(`/admin/items/show-detail-item/${itemId}`);
+        }
+    },
 
     viewBooking: (req, res) => {
         res.render('admin/booking/view_booking', {
