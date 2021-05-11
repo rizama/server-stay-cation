5c1ba573cb027ae08027e45b70684d5a01765dd5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-05-11 22:58:47 +0700

    ^ pre save mongoose, encrypt

--- models/User.js
+++ models/User.js
@@ -12,4 +12,11 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+});
+
 module.exports = mongoose.model('User', userSchema);
