3c72c6722d8b4bd891a4abb5060704f55b7ace37
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-05-18 19:05:24 +0700

    ^ middleware auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,11 @@
 const isLogin = (req, res, next) => {
-    //
-}
+    if (req.session.user == null || req.session.user == undefined) {
+        req.flash('alertMessage', 'Session telah habis, silakan login kembali');
+        req.flash('alertStatus', 'danger');
+        return res.redirect('/admin/login');
+    } 
+
+    next();
+};
+
+module.exports = isLogin;
