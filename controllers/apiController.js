3c4afedf5544f39d0630ea28bf1997aec426f710
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-05-29 12:38:23 +0700

    ^ fetch data category section

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,7 +6,7 @@ const Category = require('../models/Category');
 module.exports = {
     landingPage: async (req, res) => {
         const mostPicked = await Item.find()
-            .select('_id title country city price unit')
+            .select('_id title country city price unit imageId')
             .limit(5)
             .populate({ path: 'imageId', select: '_id imageUrl' });
 
@@ -15,14 +15,27 @@ module.exports = {
         const cities = await Item.find();
 
         const categories = await Category.find()
+            .select('_id name')
+            .limit(3)
+            .populate({
+                path: 'itemId',
+                select: '_id title country city price unit imageId isPopular',
+                perDocumentLimit: 4,
+                populate: {
+                    path: 'imageId',
+                    select: '_id imageUrl',
+                    perDocumentLimit: 1,
+                }
+            });
 
-        res.status(200).json({ 
+        res.status(200).json({
             hero: {
                 treasures: treasures.length,
                 travelers: travelers.length,
-                cities: cities.length
+                cities: cities.length,
             },
-            mostPicked, 
+            mostPicked,
+            categories
         });
     },
 };
