cd69460c769b07da75804954fc8c5537a6a4e213
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-05-29 20:25:37 +0700

    ^ API detail page

--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ const apiController = require('../controllers/apiController');
 const { uploadSingle, uploadMultiple } = require('../middleware/multer');
 
 router.get('/landingpage', apiController.landingPage);
+router.get('/detailpage/:id', apiController.detailPage);
 
 module.exports = router;
