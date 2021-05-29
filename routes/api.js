7ada1423a12f786e4b28ec2a4599ecdf99bc54dc
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-05-29 23:38:10 +0700

    ^ booking API

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const apiController = require('../controllers/apiController');
-const { uploadSingle, uploadMultiple } = require('../middleware/multer');
+const { uploadSingle } = require('../middleware/multer');
 
 router.get('/landingpage', apiController.landingPage);
 router.get('/detailpage/:id', apiController.detailPage);
+router.post('/booking', uploadSingle, apiController.bookingPage);
 
 module.exports = router;
