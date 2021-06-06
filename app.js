8c7276b3c576ea066fe1e23c4e580552a4c212d0
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-06 23:00:22 +0700

    ^ update cors

--- app.js
+++ app.js
@@ -30,7 +30,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+var allowedOrigins = ['http://localhost:3000', 'https://backend-staycation-bwa.herokuapp.com'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
